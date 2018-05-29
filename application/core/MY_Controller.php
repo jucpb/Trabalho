267f6e90fb5a2c4c023d47f126ec54811c199892
Robson Piere <robsonpiere@sescmg.com.br>
2018-05-29 16:33:03 -0300

    add my controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends REST_Controller {
+
+    
+    //herdando da biblioteca
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+
+}
